34c7f4001ab7802c2e2866885ca7fbc0aa778231
massag03 <gregorio.massara1@bbc.co.uk>
2024-03-01 16:59:42 +0000

    add AOD View and PRB

--- app/src/main/java/uk/co/bbc/smpjetpc/MainActivity.kt
+++ app/src/main/java/uk/co/bbc/smpjetpc/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -12,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,27 +22,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ui.PlayerView
-import uk.co.bbc.smpan.ProductName as VODProductName
-import uk.co.bbc.smpan.ProductVersion as VODProductVersion
+import uk.co.bbc.smpan.AODDASHPlayRequestBuilder
+import uk.co.bbc.smpan.AODProductName
+import uk.co.bbc.smpan.AODProductVersion
 import uk.co.bbc.smpan.SMP
 import uk.co.bbc.smpan.SMPBuilder
 import uk.co.bbc.smpan.VODPlayRequestBuilder
 import uk.co.bbc.smpan.media.PlayRequest
 import uk.co.bbc.smpan.media.model.MediaContentHoldingImage
 import uk.co.bbc.smpan.stats.ui.UserInteractionStatisticsProvider
-import uk.co.bbc.smpan.videosimulcast.mediation.ProductName as SimulcastName
-import uk.co.bbc.smpan.videosimulcast.mediation.ProductVersion as SimulcastProductVersion
 import uk.co.bbc.smpan.videosimulcast.mediation.VideoSimulcastPlayRequestBuilder
 import uk.co.bbc.smpjetpc.ui.theme.SMPJetpcTheme
+import uk.co.bbc.smpan.ProductName as VODProductName
+import uk.co.bbc.smpan.ProductVersion as VODProductVersion
+import uk.co.bbc.smpan.videosimulcast.mediation.ProductName as SimulcastName
+import uk.co.bbc.smpan.videosimulcast.mediation.ProductVersion as SimulcastProductVersion
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,17 +71,18 @@ fun SMPPlayer(modifier: Modifier = Modifier) {
     Column(
         Modifier
             .fillMaxWidth()
-            .fillMaxHeight()) {
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState())) {
         Text(
             "Video Simulcast",
             modifier = Modifier
                 .padding(10.dp)
-                .align(Alignment.CenterHorizontally),
+                .align(Alignment.End)
+                .background(Color.Cyan),
             style = TextStyle(
                 fontFamily = FontFamily.Monospace,
                 fontWeight = FontWeight.W900,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
+                fontSize = 18.sp
             )
         )
         AndroidView(
@@ -98,13 +105,12 @@ fun SMPPlayer(modifier: Modifier = Modifier) {
             "VOD",
             modifier = Modifier
                 .padding(10.dp)
-                .align(Alignment.CenterHorizontally),
+                .align(Alignment.End)
+                .background(Color.Yellow),
             style = TextStyle(
                 fontFamily = FontFamily.Monospace,
                 fontWeight = FontWeight.W900,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
-            )
+                fontSize = 18.sp)
         )
 
         AndroidView(
@@ -122,15 +128,15 @@ fun SMPPlayer(modifier: Modifier = Modifier) {
         )
 
         Text(
-            "Audio Simulcast",
+            "AOD",
             modifier = Modifier
                 .padding(10.dp)
-                .align(Alignment.CenterHorizontally),
+                .align(Alignment.End)
+                .background(Color.Green),
             style = TextStyle(
                 fontFamily = FontFamily.Monospace,
                 fontWeight = FontWeight.W900,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
+                fontSize = 18.sp
             )
         )
 
@@ -138,7 +144,7 @@ fun SMPPlayer(modifier: Modifier = Modifier) {
             factory = { context ->
                 PlayerView(context).also {
                     val smp = createSMP(context)
-                    val playRequest = playVODRequest(context)
+                    val playRequest = playAODRequest(context)
                     val embeddedPlayoutWindow = smp.embeddedPlayoutWindow(playRequest)
                     embeddedPlayoutWindow.attachToViewGroup(it)
                 }
@@ -162,8 +168,8 @@ private fun playSimulcastRequest(context: Context): PlayRequest? {
 
     return VideoSimulcastPlayRequestBuilder(
         context,
-        SimulcastName("My SMP App"),
-        SimulcastProductVersion("0.0.1")
+        SimulcastName("Video Simulcast"),
+        SimulcastProductVersion("2.0.1")
     )
         .forVpid(vpid, avStatisticsProvider)
         .with(MediaContentHoldingImage("https://i.pinimg.com/originals/d9/b3/ae/d9b3ae779f3c1b95b2dbac89327924d1.jpg"))
@@ -177,8 +183,23 @@ private fun playVODRequest(context: Context): PlayRequest? {
 
     return VODPlayRequestBuilder(
         context,
-        VODProductName("My SMP App"),
-        VODProductVersion("0.0.1")
+        VODProductName("VOD PRB"),
+        VODProductVersion("4.0.1")
+    )
+        .forVpid(vpid, avStatisticsProvider)
+        .with(MediaContentHoldingImage("https://i.pinimg.com/originals/d9/b3/ae/d9b3ae779f3c1b95b2dbac89327924d1.jpg"))
+        .withAutoplay(true)
+        .build()
+}
+
+private fun playAODRequest(context: Context): PlayRequest? {
+    val avStatisticsProvider = SMPAVStatisticsProvider()
+    val vpid = "m001whh8" // Giles Peterson
+
+    return AODDASHPlayRequestBuilder(
+        context,
+        AODProductName("AOD PRB"),
+        AODProductVersion("3.0.1")
     )
         .forVpid(vpid, avStatisticsProvider)
         .with(MediaContentHoldingImage("https://i.pinimg.com/originals/d9/b3/ae/d9b3ae779f3c1b95b2dbac89327924d1.jpg"))
